8a2c38e27e06d9ad00c1df12a8e63c1c38da8678
Tammy Peters <tamarapeters2@gmail.com>
2015-09-04 15:13:03 -0400

    add links to work page, modify footer

--- js/app.js
+++ js/app.js
@@ -33,3 +33,13 @@ $(function(){
         return false;
     });
 })
+
+var elementPosition = $('#sticky-nav').offset();
+
+$(document).scroll(function() {
+    if($(window).scrollTop() > elementPosition.top){
+        $('#sticky-nav').addClass('navbar-fixed-top');
+    } else {
+        $('#sticky-nav').removeClass('navbar-fixed-top');
+    }
+});
