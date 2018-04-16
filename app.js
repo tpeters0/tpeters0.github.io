c7721cc2ac634e0f39b9cce64bb7d190c436d0e3
Tammy_Peters <tamarapeters2@gmail.com>
2018-04-16 02:31:19 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -11,5 +11,5 @@
 function overlay() {
 	el = document.getElementById("overlay");
 	el.style.visibility = "hidden";
-	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+	//el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
 }
