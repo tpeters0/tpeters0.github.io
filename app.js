52c5b42a10d4994ad9147977c66d903df40f2040
Tammy_Peters <tamarapeters2@gmail.com>
2018-04-16 02:23:14 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+// var elementPosition = $('#sticky-nav').offset();
+//
+// $(document).scroll(function() {
+//     if($(window).scrollTop() > elementPosition.top){
+//         $('#sticky-nav').addClass('navbar-fixed-top');
+//     } else {
+//         $('#sticky-nav').removeClass('navbar-fixed-top');
+//     }
+// });
+
+function overlay() {
+	el = document.getElementById("overlay");
+	el.style.visibility = false;
+	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+}
